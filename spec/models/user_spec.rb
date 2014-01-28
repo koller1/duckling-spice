6d2281f64ddf5833d88a1ac6e9d007bf954bd2fb
Alex Koller <koller1@live.com>
2014-01-27 19:07:13 -0600

    Created user model, indexed the email and ran db migrations.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe User do
+	before { @user = User.new(name: "Exmaple User", email: "user@example.com" ) }
+
+	subject { @user }
+
+	it { should respond_to(:name) }
+	it { should respond_to(:email) }
+
+	it { should be_valid }
+
+	describe "when name is not present" do
+		before { @user.name = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
+
+	describe "when name is too long" do
+		before { @user.name = "a" * 51 }
+		it { should_not be_valid }
+	end
+
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save # Save dup to DB to test @user is not valid?
+		end
+
+		it { should_not be_valid }
+	end
+end
