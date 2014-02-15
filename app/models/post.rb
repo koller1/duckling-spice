150dbf68e93e22791d805c7a54d7636d2e29988c
Alex Koller <koller1@live.com>
2014-02-14 18:38:28 -0600

    Added tagging to Posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
 class Post < ActiveRecord::Base
+	before_validation :add_tags, on: :create
+
   belongs_to :user
   validates :user_id, presence: true
+
+	acts_as_taggable # Alias for acts_as_taggable_on :tags
+
+	private
+		def add_tags
+			self.text.split.each do |word|
+				self.tag_list << word if word.first == '#'
+			end
+		end
 end
